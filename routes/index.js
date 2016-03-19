45bd42497e58fadad923c744c0a2ee4a5f7cd88b
mxi75 <mxi75@icloud.com>
2016-03-19 12:16:04 +0100

    Add index info

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var globals = require('../lib/globals');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Photoalbums' });
+  var db = globals.database();
+  res.render('index', {
+    title: 'Photoalbums',
+    db: db
+  });
 });
 
 module.exports = router;
